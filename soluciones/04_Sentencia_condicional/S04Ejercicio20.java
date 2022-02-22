6c70947c6a7755e02ba8e13ab63ed34f140772ae
Dorime9175 <88950143+Dorime9175@users.noreply.github.com>
2022-02-21 22:12:31 -0600

    Update S04Ejercicio20.java

--- soluciones/04_Sentencia_condicional/S04Ejercicio20.java
+++ soluciones/04_Sentencia_condicional/S04Ejercicio20.java
@@ -4,8 +4,6 @@
  * 20. Realiza un programa que diga si un número entero positivo
  *     introducido por teclado es capicúa. Se permiten números de
  *     hasta 5 cifras.
- *
- * @author Luis José Sánchez
  */
 public class S04Ejercicio20 {
 
