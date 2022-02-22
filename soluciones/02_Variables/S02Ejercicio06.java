6e3bfeef29b1a8b691613737f7d5ff877f84543a
Dorime9175 <88950143+Dorime9175@users.noreply.github.com>
2022-02-21 22:02:30 -0600

    Update S02Ejercicio06.java

--- soluciones/02_Variables/S02Ejercicio06.java
+++ soluciones/02_Variables/S02Ejercicio06.java
@@ -4,8 +4,6 @@
  * 6. Escribe un programa que calcule el total de una factura a partir
  *    de la base imponible (precio sin IVA). La base imponible estará
  *    almacenada en una variable.
- *
- * @author Luis José Sánchez
  */
 public class S02Ejercicio06 {
   public static void main(String[] args) {
