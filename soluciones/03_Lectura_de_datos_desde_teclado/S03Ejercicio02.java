ef5de650acee0f88a7d61a0dd3554b89b9e5a7e1
Dorime9175 <88950143+Dorime9175@users.noreply.github.com>
2022-02-21 22:04:02 -0600

    Update S03Ejercicio02.java

--- soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio02.java
+++ soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio02.java
@@ -4,7 +4,7 @@
  * 2. Realiza un conversor de euros a pesetas. La cantidad de euros que
  *    se quiere convertir debe ser introducida por teclado.
  *
- * @author Luis José Sánchez
+
  */
 public class S03Ejercicio02 {
   public static void main(String[] args) {
