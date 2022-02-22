466a68610a22c6dc997fb8de517a26408d4c3e79
Dorime9175 <88950143+Dorime9175@users.noreply.github.com>
2022-02-21 22:01:52 -0600

    Update S02Ejercicio03.java

--- soluciones/02_Variables/S02Ejercicio03.java
+++ soluciones/02_Variables/S02Ejercicio03.java
@@ -5,8 +5,6 @@
  *    valores correspondientes. Muestra los valores de esas variables
  *    por pantalla de tal forma que el resultado del programa sea el
  *    mismo que en el ejercicio 2 del capítulo 1.
- *
- * @author Luis José Sánchez
  */
 public class S02Ejercicio03 {
   public static void main(String[] args) {
