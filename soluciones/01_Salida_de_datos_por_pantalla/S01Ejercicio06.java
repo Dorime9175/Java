9454020d0d2e0950e3285b7ce9736655359ca559
Dorime9175 <88950143+Dorime9175@users.noreply.github.com>
2022-02-21 21:59:26 -0600

    Update S01Ejercicio06.java

--- soluciones/01_Salida_de_datos_por_pantalla/S01Ejercicio06.java
+++ soluciones/01_Salida_de_datos_por_pantalla/S01Ejercicio06.java
@@ -4,8 +4,6 @@
  * 6. Escribe un programa que pinte por pantalla una pirámide rellena a
  *    base de asteriscos. La base de la pirámide debe estar formada por
  *    9 asteriscos.
- *
- * @author Luis José Sánchez
  */
 public class S01Ejercicio06 {
   public static void main(String[] args) {
