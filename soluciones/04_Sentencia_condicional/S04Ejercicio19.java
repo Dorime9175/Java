fc0982bb0a9b1401e4bb9d1b98fc4cf66135d64e
Dorime9175 <88950143+Dorime9175@users.noreply.github.com>
2022-02-21 22:12:03 -0600

    Update S04Ejercicio19.java

--- soluciones/04_Sentencia_condicional/S04Ejercicio19.java
+++ soluciones/04_Sentencia_condicional/S04Ejercicio19.java
@@ -4,8 +4,6 @@
  * 19. Realiza un programa que nos diga cuántos dígitos tiene un número
  *     entero que puede ser positivo o negativo. Se permiten números de
  *     hasta 5 dígitos.
- *
- * @author Luis José Sánchez
  */
 public class S04Ejercicio19 {
 
