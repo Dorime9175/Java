4d15c34316d615c8156d10a0267ed4d7cec0872a
Dorime9175 <88950143+Dorime9175@users.noreply.github.com>
2022-02-21 22:01:28 -0600

    Update S02Ejercicio01.java

--- soluciones/02_Variables/S02Ejercicio01.java
+++ soluciones/02_Variables/S02Ejercicio01.java
@@ -5,8 +5,6 @@
  *    e y. Asignales los valores 144 y 999 respectivamente. A continuación,
  *    muestra por pantalla el valor de cada variable, la suma, la resta,
  *    la división y la multiplicación.
- *
- * @author Luis José Sánchez
  */
 public class S02Ejercicio01 {
   public static void main(String[] args) {
