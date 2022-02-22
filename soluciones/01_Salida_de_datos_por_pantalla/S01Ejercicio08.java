dc29fd1267aa574793b21b7c66330d0ac2556889
Dorime9175 <88950143+Dorime9175@users.noreply.github.com>
2022-02-21 21:59:59 -0600

    Update S01Ejercicio08.java

--- soluciones/01_Salida_de_datos_por_pantalla/S01Ejercicio08.java
+++ soluciones/01_Salida_de_datos_por_pantalla/S01Ejercicio08.java
@@ -3,9 +3,6 @@
  *
  * 8. Igual que el programa anterior, pero esta vez la pirámide debe
  *    aparecer invertida, con el vértice hacia abajo.
- *
- * @author Luis José Sánchez
- * 
  */
 public class S01Ejercicio08 {
   public static void main(String[] args) {
