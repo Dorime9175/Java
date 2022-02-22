ead5d3d5ce4afac53b48b1cf2f65b5440cb3fcb8
Dorime9175 <88950143+Dorime9175@users.noreply.github.com>
2022-02-21 21:59:45 -0600

    Update S01Ejercicio07.java

--- soluciones/01_Salida_de_datos_por_pantalla/S01Ejercicio07.java
+++ soluciones/01_Salida_de_datos_por_pantalla/S01Ejercicio07.java
@@ -3,9 +3,6 @@
  *
  * 7. Igual que el programa anterior, pero esta vez la pirámide estará
  *    hueca (se debe ver únicamente el contorno hecho con asteriscos).
- *
- * @author Luis José Sánchez
- * 
  */
 public class S01Ejercicio07 {
   public static void main(String[] args) {
