5d94a2362835cac460e8f2e1fe12be356a937a21
Dorime9175 <88950143+Dorime9175@users.noreply.github.com>
2022-02-21 21:46:14 -0600

    Update CaracteresEspeciales02.java

--- ejemplos/01_Hola_mundo_Salida_de_datos_por_pantalla/CaracteresEspeciales02.java
+++ ejemplos/01_Hola_mundo_Salida_de_datos_por_pantalla/CaracteresEspeciales02.java
@@ -1,8 +1,4 @@
-/**
- * Muestra caracteres especiales
- *
- * @author Luis J. Sánchez
- */
+
 public class CaracteresEspeciales02 {
   public static void main(String[] args) {
     System.out.println("Una corchea: \u266A");
