c7380dae882a977038dfdda9cd3df5a972c43376
Dorime9175 <88950143+Dorime9175@users.noreply.github.com>
2022-02-21 22:06:06 -0600

    Update S03Ejercicio11.java

--- soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio11.java
+++ soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio11.java
@@ -2,8 +2,6 @@
  * 3. Lectura de datos desde teclado
  *
  * 11. Realiza un conversor de Kb a Mb.
- *
- * @author Luis José Sánchez
  */
 public class S03Ejercicio11 {
   public static void main(String[] args) {
