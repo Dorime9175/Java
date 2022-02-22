8fcecdcce7b2f91922096400a648696915b16fdd
Dorime9175 <88950143+Dorime9175@users.noreply.github.com>
2022-02-21 22:07:54 -0600

    Update S04Ejercicio01.java

--- soluciones/04_Sentencia_condicional/S04Ejercicio01.java
+++ soluciones/04_Sentencia_condicional/S04Ejercicio01.java
@@ -3,8 +3,7 @@
  *
  * 1. Escribe un programa que pida por teclado un día de la semana
  *    y que diga qué asignatura toca a primera hora ese día.
- *
- * @author Luis José Sánchez
+
  */
 public class S04Ejercicio01 {
   public static void main(String[] args) {
