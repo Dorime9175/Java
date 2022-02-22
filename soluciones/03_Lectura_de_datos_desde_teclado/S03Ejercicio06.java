7ca56adad80f394ea48c86e2da2196600638b42d
Dorime9175 <88950143+Dorime9175@users.noreply.github.com>
2022-02-21 22:04:56 -0600

    Update S03Ejercicio06.java

--- soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio06.java
+++ soluciones/03_Lectura_de_datos_desde_teclado/S03Ejercicio06.java
@@ -2,8 +2,6 @@
  * 3. Lectura de datos desde teclado
  *
  * 6. Escribe un programa que calcule el área de un triángulo.
- *
- * @author Luis José Sánchez
  */
 public class S03Ejercicio06 {
   public static void main(String[] args) {
