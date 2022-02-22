c9efbfd7c3aa781e11b206970fd8b23e62425545
Dorime9175 <88950143+Dorime9175@users.noreply.github.com>
2022-02-21 22:08:20 -0600

    Update S04Ejercicio03.java

--- soluciones/04_Sentencia_condicional/S04Ejercicio03.java
+++ soluciones/04_Sentencia_condicional/S04Ejercicio03.java
@@ -3,8 +3,6 @@
  *
  * 3. Escribe un programa en que dado un número del 1 a 7 escriba el
  *    correspondiente nombre del día de la semana.
- *
- * @author Luis José Sánchez
  */
 
 public class S04Ejercicio03 {
