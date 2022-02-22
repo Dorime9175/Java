f10f2ae05d14b74fc30db6d81d171e0b3be97ca4
Dorime9175 <88950143+Dorime9175@users.noreply.github.com>
2022-02-21 21:45:51 -0600

    Update CaracteresEspeciales01.java

--- ejemplos/01_Hola_mundo_Salida_de_datos_por_pantalla/CaracteresEspeciales01.java
+++ ejemplos/01_Hola_mundo_Salida_de_datos_por_pantalla/CaracteresEspeciales01.java
@@ -1,8 +1,4 @@
-/**
- * Muestra caracteres especiales
- *
- * @author Luis J. Sánchez
- */
+
 public class CaracteresEspeciales01 {
   public static void main(String[] args) {
     System.out.print("Cuando programo ⌨ estoy");
